2fe81dda0dc3bbacf3677a50472efe01dec72249
Jakub Knap <jknap40@gmail.com>
2023-06-20 21:18:38 +0200

    Stronicowanie produktów

--- src/main/java/pl/knap/shop/product/service/ProductService.java
+++ src/main/java/pl/knap/shop/product/service/ProductService.java
@@ -1,11 +1,11 @@
 package pl.knap.shop.product.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import pl.knap.shop.product.model.Product;
 import pl.knap.shop.product.repository.ProductRepository;
-
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -13,7 +13,7 @@ public class ProductService {
 
     private final ProductRepository productRepository;
 
-    public List<Product> getProducts() {
-        return productRepository.findAll();
+    public Page<Product> getProducts(Pageable pageable) {
+        return productRepository.findAll(pageable);
     }
 }
