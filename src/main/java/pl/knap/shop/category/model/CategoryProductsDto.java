21abe3471392d47cacaa96f16a4f19307e0cb568
Jakub Knap <jknap40@gmail.com>
2023-07-11 21:26:19 +0200

    Mapowanie encji do DTO

--- src/main/java/pl/knap/shop/category/model/CategoryProductsDto.java
+++ src/main/java/pl/knap/shop/category/model/CategoryProductsDto.java
@@ -1,7 +1,7 @@
 package pl.knap.shop.category.model;
 
 import org.springframework.data.domain.Page;
-import pl.knap.shop.product.model.Product;
+import pl.knap.shop.product.controller.dto.ProductListDto;
 
-public record CategoryProductsDto(Category category, Page<Product> products) {
+public record CategoryProductsDto(Category category, Page<ProductListDto> products) {
 }
--- src/main/java/pl/knap/shop/category/service/CategoryService.java
+++ src/main/java/pl/knap/shop/category/service/CategoryService.java
@@ -2,12 +2,14 @@ package pl.knap.shop.category.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import pl.knap.shop.category.model.Category;
 import pl.knap.shop.category.model.CategoryProductsDto;
 import pl.knap.shop.category.repository.CategoryRepository;
+import pl.knap.shop.product.controller.dto.ProductListDto;
 import pl.knap.shop.product.model.Product;
 import pl.knap.shop.product.repository.ProductRepository;
 
@@ -28,6 +30,16 @@ public class CategoryService {
     public CategoryProductsDto getCategoriesWithProducts(String slug, Pageable pageable) {
         Category category = categoryRepository.findBySlug(slug);
         Page<Product> page = productRepository.findAllByCategoryId(category.getId(), pageable);
-        return new CategoryProductsDto(category, page);
+        List<ProductListDto> productListDtos = page.getContent().stream().map(product -> ProductListDto.builder()
+                        .id(product.getId())
+                        .name(product.getName())
+                        .description(product.getDescription())
+                        .price(product.getPrice())
+                        .currency(product.getCurrency())
+                        .image(product.getImage())
+                        .slug(product.getSlug())
+                        .build())
+                .toList();
+        return new CategoryProductsDto(category, new PageImpl<>(productListDtos, pageable, page.getTotalElements()));
     }
 }
