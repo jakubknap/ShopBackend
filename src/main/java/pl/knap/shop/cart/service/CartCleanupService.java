b88fa8bc424577684fa7bed16fe8d1752ca8cb5f
Jakub Knap <jknap40@gmail.com>
2023-07-18 21:08:04 +0200

    Czyszczenie starych koszyków

---
+++ src/main/java/pl/knap/shop/cart/service/CartCleanupService.java
@@ -0,0 +1,34 @@
+package pl.knap.shop.cart.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import pl.knap.shop.cart.model.Cart;
+import pl.knap.shop.cart.repository.CartItemRepository;
+import pl.knap.shop.cart.repository.CartRepository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class CartCleanupService {
+
+    private final CartRepository cartRepository;
+    private final CartItemRepository cartItemRepository;
+
+    @Transactional
+    @Scheduled(cron = "${app.cart.cleanup.expression}")
+    public void cleanupOldCarts() {
+        List<Cart> carts = cartRepository.findByCreatedLessThan(LocalDateTime.now().minusDays(3));
+        List<Long> ids = carts.stream().map(Cart::getId).toList();
+        log.info("Stare koszyki {}", carts.size());
+        if (!ids.isEmpty()) {
+            cartItemRepository.deleteAllByCartIdIn(ids);
+            cartRepository.deleteAllByIdIn(ids);
+        }
+    }
+}
