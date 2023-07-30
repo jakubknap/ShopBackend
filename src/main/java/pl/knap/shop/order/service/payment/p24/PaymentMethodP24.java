375a3267d6f913cd3bfd863006a4325c29c128e7
Jakub Knap <jknap40@gmail.com>
2023-07-30 18:28:48 +0200

    Review modułu

--- src/main/java/pl/knap/shop/order/service/payment/p24/PaymentMethodP24.java
+++ src/main/java/pl/knap/shop/order/service/payment/p24/PaymentMethodP24.java
@@ -2,17 +2,15 @@ package pl.knap.shop.order.service.payment.p24;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.codec.digest.DigestUtils;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.reactive.function.client.ExchangeFilterFunctions;
 import org.springframework.web.reactive.function.client.WebClient;
 import pl.knap.shop.order.model.Order;
 import pl.knap.shop.order.model.dto.NotificationReceiveDto;
 import reactor.core.publisher.Mono;
 
-import java.math.BigDecimal;
+import static pl.knap.shop.order.service.payment.p24.RequestUtil.*;
 
 @Service
 @RequiredArgsConstructor
@@ -20,37 +18,14 @@ import java.math.BigDecimal;
 public class PaymentMethodP24 {
 
     private final PaymentMethodP24Config config;
+    private final WebClient p24Client;
 
     public String initPayment(Order order) {
         log.info("Inicjalizacja płatności");
 
-        WebClient webClient = WebClient.builder()
-                                       .filter(ExchangeFilterFunctions.basicAuthentication(config.getPosId()
-                                                                                                 .toString(),
-                                                                                           config.isTestMode() ? config.getTestSecretKey() : config.getSecretKey()))
-                                       .baseUrl(config.isTestMode() ? config.getTestApiUrl() : config.getApiUrl())
-                                       .build();
-
-        ResponseEntity<TransactionRegisterResponse> result = webClient.post()
+        ResponseEntity<TransactionRegisterResponse> result = p24Client.post()
                                                                       .uri("/transaction/register")
-                                                                      .bodyValue(TransactionRegisterRequest.builder()
-                                                                                                           .merchantId(config.getMerchantId())
-                                                                                                           .posId(config.getPosId())
-                                                                                                           .sessionId(createSessionId(order))
-                                                                                                           .amount(order.getGrossValue()
-                                                                                                                        .movePointRight(2)
-                                                                                                                        .intValue())
-                                                                                                           .currency("PLN")
-                                                                                                           .description("Zamówienie id: " + order.getId())
-                                                                                                           .email(order.getEmail())
-                                                                                                           .client(order.getFirstname() + " " + order.getLastname())
-                                                                                                           .country("PL")
-                                                                                                           .language("pl")
-                                                                                                           .urlReturn(generateReturnUrl(order.getOrderHash()))
-                                                                                                           .urlStatus(generateStatusUrl(order.getOrderHash()))
-                                                                                                           .sign(createSign(order))
-                                                                                                           .encoding("UTF-8")
-                                                                                                           .build())
+                                                                      .bodyValue(createTransactionRegisterRequest(config, order))
                                                                       .retrieve()
                                                                       .onStatus(HttpStatusCode::is4xxClientError,
                                                                                 clientResponse -> {
@@ -69,65 +44,17 @@ public class PaymentMethodP24 {
         return null;
     }
 
-    private String generateStatusUrl(String orderHash) {
-        String baseUrl = config.isTestMode() ? config.getTestUrlStatus() : config.getUrlStatus();
-        return baseUrl + "/orders/notification/" + orderHash;
-    }
-
-    private String generateReturnUrl(String orderHash) {
-        String baseUrl = config.isTestMode() ? config.getTestApiUrl() : config.getApiUrl();
-        return baseUrl + "/order/notification/" + orderHash;
-    }
-
-    private String createSign(Order order) {
-        String json = "{\"sessionId\":\"" + createSessionId(order) +
-                      "\",\"merchantId\":" + config.getMerchantId() +
-                      ",\"amount\":" + order.getGrossValue()
-                                            .movePointRight(2)
-                                            .intValue() +
-                      ",\"currency\":\"PLN\",\"crc\":\"" + (config.isTestMode() ? config.getTestCrc() : config.getCrc()) + "\"}";
-        return DigestUtils.sha384Hex(json);
-    }
-
-    private String createSessionId(Order order) {
-        return "order_id_" + order.getId()
-                                  .toString();
-    }
-
     public String receiveNotification(Order order, NotificationReceiveDto receiveDto, String remoteAddr) {
         log.info(receiveDto.toString());
-        validateIpAddress(remoteAddr);
-        validate(receiveDto, order);
+        validateIpAddress(remoteAddr, config);
+        validate(receiveDto, order, config);
         return verifyPayment(receiveDto, order);
     }
 
-    private void validateIpAddress(String remoteAddr) {
-        if (!config.getServers()
-                   .contains(remoteAddr)) {
-            throw new RuntimeException("Niepoprawny adres IP dla potwierdzenia płatności");
-        }
-    }
-
     private String verifyPayment(NotificationReceiveDto receiveDto, Order order) {
-        WebClient webClient = WebClient.builder()
-                                       .filter(ExchangeFilterFunctions.basicAuthentication(config.getPosId()
-                                                                                                 .toString(),
-                                                                                           config.isTestMode() ? config.getTestSecretKey() : config.getSecretKey()))
-                                       .baseUrl(config.isTestMode() ? config.getTestApiUrl() : config.getApiUrl())
-                                       .build();
-        ResponseEntity<TransactionVerifyResponse> result = webClient.put()
+        ResponseEntity<TransactionVerifyResponse> result = p24Client.put()
                                                                     .uri("/transaction/verify")
-                                                                    .bodyValue(TransactionVerifyRequest.builder()
-                                                                                                       .merchantId(config.getMerchantId())
-                                                                                                       .posId(config.getPosId())
-                                                                                                       .sessionId(createSessionId(order))
-                                                                                                       .amount(order.getGrossValue()
-                                                                                                                    .movePointRight(2)
-                                                                                                                    .intValue())
-                                                                                                       .currency("PLN")
-                                                                                                       .orderId(receiveDto.getOrderId())
-                                                                                                       .sign(createVerifySign(receiveDto, order))
-                                                                                                       .build())
+                                                                    .bodyValue(createTransactionVerifyRequest(config, order, receiveDto))
                                                                     .retrieve()
                                                                     .toEntity(TransactionVerifyResponse.class)
                                                                     .block();
@@ -137,56 +64,5 @@ public class PaymentMethodP24 {
         return result.getBody()
                      .getData()
                      .status();
-    }
-
-    private String createVerifySign(NotificationReceiveDto receiveDto, Order order) {
-        String json = "{\"sessionId\":\"" + createSessionId(order) +
-                      "\",\"orderId\":" + receiveDto.getOrderId() +
-                      ",\"amount\":" + order.getGrossValue()
-                                            .movePointRight(2)
-                                            .intValue() +
-                      ",\"currency\":\"PLN\",\"crc\":\"" + (config.isTestMode() ? config.getTestCrc() : config.getCrc()) + "\"}";
-        return DigestUtils.sha384Hex(json);
-    }
-
-    private void validate(NotificationReceiveDto receiveDto, Order order) {
-        validateField(config.getMerchantId()
-                            .equals(receiveDto.getMerchantId()));
-        validateField(config.getPosId()
-                            .equals(receiveDto.getPosId()));
-        validateField(createSessionId(order).equals(receiveDto.getSessionId()));
-        validateField(order.getGrossValue()
-                           .compareTo(BigDecimal.valueOf(receiveDto.getAmount())
-                                                .movePointLeft(2)) == 0);
-        validateField(order.getGrossValue()
-                           .compareTo(BigDecimal.valueOf(receiveDto.getOriginAmount())
-                                                .movePointLeft(2)) == 0);
-        validateField("PLN".equals(receiveDto.getCurrency()));
-        validateField(createReceivedSign(receiveDto, order).equals(receiveDto.getSign()));
-
-    }
-
-    private String createReceivedSign(NotificationReceiveDto receiveDto, Order order) {
-        String json = "{\"merchantId\":" + config.getMerchantId() +
-                      ",\"posId\":" + config.getPosId() +
-                      ",\"sessionId\":\"" + createSessionId(order) +
-                      "\",\"amount\":" + order.getGrossValue()
-                                              .movePointRight(2)
-                                              .intValue() +
-                      ",\"originAmount\":" + order.getGrossValue()
-                                                  .movePointRight(2)
-                                                  .intValue() +
-                      ",\"currency\":\"PLN\"" +
-                      ",\"orderId\":" + receiveDto.getOrderId() +
-                      ",\"methodId\":" + receiveDto.getMethodId() +
-                      ",\"statement\":\"" + receiveDto.getStatement() +
-                      "\",\"crc\":\"" + (config.isTestMode() ? config.getTestCrc() : config.getCrc()) + "\"}";
-        return DigestUtils.sha384Hex(json);
-    }
-
-    private void validateField(boolean condition) {
-        if (!condition) {
-            throw new RuntimeException("Walidacja niepoprawna");
-        }
     }
 }
---
+++ src/main/java/pl/knap/shop/order/service/payment/p24/RequestUtil.java
@@ -0,0 +1,128 @@
+package pl.knap.shop.order.service.payment.p24;
+
+import org.apache.commons.codec.digest.DigestUtils;
+import pl.knap.shop.order.model.Order;
+import pl.knap.shop.order.model.dto.NotificationReceiveDto;
+
+import java.math.BigDecimal;
+
+public class RequestUtil {
+
+    public static TransactionRegisterRequest createTransactionRegisterRequest(PaymentMethodP24Config config, Order order) {
+        return TransactionRegisterRequest.builder()
+                                         .merchantId(config.getMerchantId())
+                                         .posId(config.getPosId())
+                                         .sessionId(createSessionId(order))
+                                         .amount(order.getGrossValue()
+                                                      .movePointRight(2)
+                                                      .intValue())
+                                         .currency("PLN")
+                                         .description("Zamówienie id: " + order.getId())
+                                         .email(order.getEmail())
+                                         .client(order.getFirstname() + " " + order.getLastname())
+                                         .country("PL")
+                                         .language("pl")
+                                         .urlReturn(generateReturnUrl(order.getOrderHash(), config))
+                                         .urlStatus(generateStatusUrl(order.getOrderHash(), config))
+                                         .sign(createSign(order, config))
+                                         .encoding("UTF-8")
+                                         .build();
+    }
+
+    public static TransactionVerifyRequest createTransactionVerifyRequest(PaymentMethodP24Config config, Order order, NotificationReceiveDto receiveDto) {
+        return TransactionVerifyRequest.builder()
+                                       .merchantId(config.getMerchantId())
+                                       .posId(config.getPosId())
+                                       .sessionId(createSessionId(order))
+                                       .amount(order.getGrossValue()
+                                                    .movePointRight(2)
+                                                    .intValue())
+                                       .currency("PLN")
+                                       .orderId(receiveDto.getOrderId())
+                                       .sign(createVerifySign(receiveDto, order, config))
+                                       .build();
+    }
+
+    public static void validateIpAddress(String remoteAddr, PaymentMethodP24Config config) {
+        if (!config.getServers()
+                   .contains(remoteAddr)) {
+            throw new RuntimeException("Niepoprawny adres IP dla potwierdzenia płatności");
+        }
+    }
+
+    public static void validate(NotificationReceiveDto receiveDto, Order order, PaymentMethodP24Config config) {
+        validateField(config.getMerchantId()
+                            .equals(receiveDto.getMerchantId()));
+        validateField(config.getPosId()
+                            .equals(receiveDto.getPosId()));
+        validateField(createSessionId(order).equals(receiveDto.getSessionId()));
+        validateField(order.getGrossValue()
+                           .compareTo(BigDecimal.valueOf(receiveDto.getAmount())
+                                                .movePointLeft(2)) == 0);
+        validateField(order.getGrossValue()
+                           .compareTo(BigDecimal.valueOf(receiveDto.getOriginAmount())
+                                                .movePointLeft(2)) == 0);
+        validateField("PLN".equals(receiveDto.getCurrency()));
+        validateField(createReceivedSign(receiveDto, order, config).equals(receiveDto.getSign()));
+
+    }
+
+    private static String generateStatusUrl(String orderHash, PaymentMethodP24Config config) {
+        String baseUrl = config.isTestMode() ? config.getTestUrlStatus() : config.getUrlStatus();
+        return baseUrl + "/orders/notification/" + orderHash;
+    }
+
+    private static String generateReturnUrl(String orderHash, PaymentMethodP24Config config) {
+        String baseUrl = config.isTestMode() ? config.getTestApiUrl() : config.getApiUrl();
+        return baseUrl + "/order/notification/" + orderHash;
+    }
+
+    private static String createSign(Order order, PaymentMethodP24Config config) {
+        String json = "{\"sessionId\":\"" + createSessionId(order) +
+                      "\",\"merchantId\":" + config.getMerchantId() +
+                      ",\"amount\":" + order.getGrossValue()
+                                            .movePointRight(2)
+                                            .intValue() +
+                      ",\"currency\":\"PLN\",\"crc\":\"" + (config.isTestMode() ? config.getTestCrc() : config.getCrc()) + "\"}";
+        return DigestUtils.sha384Hex(json);
+    }
+
+    private static String createSessionId(Order order) {
+        return "order_id_" + order.getId()
+                                  .toString();
+    }
+
+    private static String createVerifySign(NotificationReceiveDto receiveDto, Order order, PaymentMethodP24Config config) {
+        String json = "{\"sessionId\":\"" + createSessionId(order) +
+                      "\",\"orderId\":" + receiveDto.getOrderId() +
+                      ",\"amount\":" + order.getGrossValue()
+                                            .movePointRight(2)
+                                            .intValue() +
+                      ",\"currency\":\"PLN\",\"crc\":\"" + (config.isTestMode() ? config.getTestCrc() : config.getCrc()) + "\"}";
+        return DigestUtils.sha384Hex(json);
+    }
+
+    private static String createReceivedSign(NotificationReceiveDto receiveDto, Order order, PaymentMethodP24Config config) {
+        String json = "{\"merchantId\":" + config.getMerchantId() +
+                      ",\"posId\":" + config.getPosId() +
+                      ",\"sessionId\":\"" + createSessionId(order) +
+                      "\",\"amount\":" + order.getGrossValue()
+                                              .movePointRight(2)
+                                              .intValue() +
+                      ",\"originAmount\":" + order.getGrossValue()
+                                                  .movePointRight(2)
+                                                  .intValue() +
+                      ",\"currency\":\"PLN\"" +
+                      ",\"orderId\":" + receiveDto.getOrderId() +
+                      ",\"methodId\":" + receiveDto.getMethodId() +
+                      ",\"statement\":\"" + receiveDto.getStatement() +
+                      "\",\"crc\":\"" + (config.isTestMode() ? config.getTestCrc() : config.getCrc()) + "\"}";
+        return DigestUtils.sha384Hex(json);
+    }
+
+    private static void validateField(boolean condition) {
+        if (!condition) {
+            throw new RuntimeException("Walidacja niepoprawna");
+        }
+    }
+}
